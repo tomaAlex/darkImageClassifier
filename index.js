3861e6931b4c99aa4a2bc1ad43c060f9550917c6
tomaAlex <reparalex1@gmail.com>
2020-07-13 01:37:32 +0300

    solved the promises problem

--- index.js
+++ index.js
@@ -4,34 +4,44 @@ const { reject } = require('q');
 const { Console } = require('console');
 const gm = require('gm').subClass({imageMagick: true});
 const PNG = require("pngjs").PNG;
-let pathToFolder = '/wallpapers';
+let pathToFolder = '/home/eugen/Pictures/wallpapers1';
 let pathToImage = '';
 let fileNames = new Array();
+let files; // save files from given directory in global variable
+           // so that promises can execute after `readdir` method
 
-let promiseImageScore = new Promise((resolve, reject) => {
-  getImageScore(resolve, reject);
-});
+function promiseImageScore() {
+  return new Promise((resolve, reject) => {
+    getImageScore(resolve, reject);
+  }); 
+}
+
+function prmoiseSavingFileNames() {
+  return new Promise((resolve, reject) => {
+    addFileNames(resolve, reject);
+  });
+}
+
+function promiseReadingDir() {
+  return new Promise((resolve, reject) => {
+    readDir(resolve, reject);
+  });
+}
 
 function getImageScore(resolve, reject) {
-  console.log('entered this promise....');
   let img = gm(pathToImage);
-  //console.log(img);
   // Get the PNG buffer
   img.toBuffer("PNG", (err, buff) => {
     if (err) return reject(err);
-    console.log('got buffer...');
     // Get the image size
     img.size((err, size) => {
       if (err) {
         console.log(err);
         return reject(err);
       }
-      console.log('got image size...');
       // Parse the PNG buffer
-      console.log('got to the buffer');
       let str = new PNG();
       str.end(buff);
-      console.log('parsed buffer...');
       // After it's parsed...
       str.on("parsed", buffer => {
         // Get the pixels from the image
@@ -45,7 +55,6 @@ function getImageScore(resolve, reject) {
             rgb.b = buffer[idx + 2];
             score += (rgb.r + rgb.g + rgb.b) / 765;
           }
-          console.log('one promise finished...');
           return resolve(score / (size.height * size.width));
       });
       str.on("error", e => {
@@ -55,12 +64,13 @@ function getImageScore(resolve, reject) {
   });
 }
 
-async function logAllTheScores() {
+function logAllTheScores() {
     for(let i = 0; i < fileNames.length; i++) {
         pathToImage = fileNames[i];
-        promiseImageScore
+        let localPathToImage = pathToImage;
+        promiseImageScore()
         .then(imageScore => {
-          console.log(file + ' has a score of ' + imageScore);
+          console.log(localPathToImage + ' has a score of ' + imageScore);
         })
         .catch(e => {
           throw e;
@@ -68,23 +78,39 @@ async function logAllTheScores() {
     }
 }
 
-// see which images are to be found in the specificd directory
-fs.readdir(pathToFolder, function (err, files) {
-    if (err) return console.log('Unable to scan directory: ' + err);
-    console.log('files in directory:\n');
-    files.forEach(function (file) {
-        pathToImage = pathToFolder + '/' + file;
-        fileNames.push(pathToImage);
-        //showImageScore();
-        /*
-        promiseImageScore
-        .then(imageScore => {
-          console.log(file + ' has a score of ' + imageScore);
-        })
-        .catch(e => {
-          throw e;
-        })
-        */
-    });
+function addFileNames(resolve, reject) {
+  files.forEach(file => {
+    pathToImage = pathToFolder + '/' + file;
+    fileNames.push(pathToImage);
+    if(files.length == fileNames.length) {
+      resolve();
+      return;
+    }
+  });
+}
+
+function readDir(resolve, reject) {
+  // see which images are to be found in the specificd directory
+  fs.readdir(pathToFolder, (err, Files) => {
+      if (err) return reject('Unable to scan directory: ' + err);
+      files = Files;
+      if(files) resolve();
+      else reject(new Error('could not load files from the given directory path...'));
+  });
+}
+
+promiseReadingDir()
+.then(() => {
+  console.log('finished reading dir files...');
+  prmoiseSavingFileNames()
+  .then(() => {
+    console.log('saved all the file names!');
     logAllTheScores();
+  })
+  .catch(err => {
+    throw err;
+  });
+})
+.catch(err => {
+  throw err;
 });
